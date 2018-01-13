634422f4a20bc0cc0d4950b91cefc80165be4b39
Mateo Llanes <microbestyles@gmail.com>
2018-01-13 22:52:41 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,111 @@
+import discord
+import asyncio
+import goodnatt as gnatt
+import random
+import shlex
+import sys
+
+client = discord.Client()
+
+#Bot variables
+presence = 'you yoo'
+help_string = """
+**shitty documentation:
+
+`!goodnatt [number of emojis]` - Sends a bunch of wholesome emojis :sparkling_heart:
+`!sponge [text]` - i aM A fuCkinG dEGenErate :100:
+`!purge [num of messages] [specified string]` - Deletes a bunch of messages (you gotta be authorized to use this) :no_good::skin-tone-2:
+**
+"""
+authorized_roles = ['399702651278983168', '399702073924517888']
+
+@client.event
+async def on_ready():
+    'Do something on startup'
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+    await client.change_presence(game=discord.Game(name=presence))
+
+@client.event
+async def on_message(message):
+    """Do something on message"""
+    if message.content.startswith('!help'):
+        await help(message)
+    elif message.content.startswith('!goodnatt'):
+        await goodnatt(message)
+    elif message.content.startswith('!sponge'):
+        await sponge(message)
+    elif message.content.startswith('!purge'):
+        #Check if user is authorized
+        auth = False
+        for r in message.author.roles:
+            if r.id in authorized_roles:
+                auth = True
+                
+        if auth:
+            #If so, do the procedure
+            #add deleting 
+            await client.send_message(message.channel, ":white_check_mark:")
+            c = shlex.split(message.content)
+            try:
+                n = int(c[1])
+                s = c[2]
+                await client.send_message(message.channel, "**Deleting {} messages containing '{}'...**".format(n, s))
+                await purge(message, n, s)
+            except IndexError:
+                try:
+                    n = int(c[1])
+                    await client.send_message(message.channel, "**Deleting {} messages...**".format(n))
+                    await purge(message, n)
+                except IndexError:
+                    await client.send_message(message.channel, "**Deleting {} messages...**".format(10))
+                    await purge(message)
+        else:
+            #If not, send a message indicating they aren't authorized
+            await client.send_message(message.channel, ":no_good::skin-tone-2:")
+
+@client.event
+async def on_member_join(member):
+    """Do something when somebody joins the server"""
+    #Assign normie role to new user
+    role = discord.utils.get(member.server.roles, name='normie')
+    await client.add_roles(member, role)
+
+async def help(message):
+    """Sends help"""
+    await client.send_message(message.channel, help_string)
+
+async def goodnatt(message):
+    """Sends a bunch of wholesome emojis"""
+    try:
+        #Try to get number of emojis specified
+        n = int(shlex.split(message.content)[1])
+        #Send message with n emojis
+        await client.send_message(message.channel, gnatt.goodnatt(gnatt.emoji_list, n))
+    except Exception:
+        #Else, send a message anyways with 10 emojis (default)
+        await client.send_message(message.channel, gnatt.goodnatt(gnatt.emoji_list, 10))
+
+async def sponge(message):
+    """gEnErATEs SpONge TeXt aND SEnds iT"""
+    sponge_text = "".join(random.choice([k.upper(), k ]) for k in message.content[7:])
+    if sponge_text: 
+        await client.send_message(message.channel, sponge_text)
+    await client.send_file(message.channel, 'assets\sponge.jpg')
+
+async def purge(message, num_msgs=10, s=""):
+    """Deletes a bulk of messages"""
+
+    msgs = []
+    async for x in client.logs_from(message.channel, limit = num_msgs):
+        if s in x.content: 
+            msgs.append(x)
+    
+    await client.delete_messages(msgs)
+
+if __name__ == '__main__':
+    token = sys.argv[1] if len(sys.argv) > 1 else sys.exit(0)
+    client.run(str(token))
---
+++ goodnatt.py
@@ -0,0 +1,25 @@
+import random
+
+#Default emoji list we feed to the function
+emoji_list = [  
+                "gay_pride_flag", "rainbow", "kiss_ww","couple_ww", "family_wwbb", "family_wwgg", 
+                "sparkling_heart", "heart", "blue_heart", "purple_heart", "ok_woman", "star2",
+                "peach", "shaved_ice", "unicorn", "bee", "dragon", "four_leaf_clover", "hibiscus", "maple_leaf",
+                "sunflower", "white_flower", "wilted_rose", "fallen_leaf", "cherry_blossom", "tulip", "blossom",
+                "blossom", "cherry_blossom", "hibiscus", "sun_with_face", "two_hearts", "two_women_holding_hands"
+            ]
+
+def goodnatt(emojis=emoji_list, n_emojis=10):
+    'Generates a sequence of n_emoji length with emojis list of emoji names'
+    goodnatt_s = []
+
+    for i in range(n_emojis):
+        choice = random.uniform(0, 1)
+        if choice <= 0.7:
+            goodnatt_s.append(random.choice(emojis))
+        elif choice <= 0.85:
+            goodnatt_s.append("gay_pride_flag")
+        else:
+            goodnatt_s.append("sparkling_heart")
+
+    return ':' + '::'.join(goodnatt_s) + ':'
